a380dfa262241d4f58c9a8beeaca090fa2ddad94
Kwangsoo Yeo <kyeo@picovoice.ai>
2022-04-12 10:34:26 -0700

    clean repo

--- zip.sh
+++ zip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm serverless_leopard.zip
 pip install pvleopard --target ./serverless_leopard
 cd serverless_leopard && zip -r ../serverless_leopard.zip ./ && cd ..
 rm -rf ./serverless_leopard/pvleopard*
